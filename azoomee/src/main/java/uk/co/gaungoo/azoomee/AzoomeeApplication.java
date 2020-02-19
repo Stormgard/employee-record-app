f2885d49b371cd0b059441f0e6476b759b1b8412
Hass <Hass@Casper.connect>
2020-02-19 21:53:06 +0000

    First Commit

---
+++ azoomee/src/main/java/uk/co/gaungoo/azoomee/AzoomeeApplication.java
@@ -0,0 +1,13 @@
+package uk.co.gaungoo.azoomee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzoomeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzoomeeApplication.class, args);
+	}
+
+}
---
+++ azoomee/src/test/java/uk/co/gaungoo/azoomee/AzoomeeApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.gaungoo.azoomee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzoomeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
